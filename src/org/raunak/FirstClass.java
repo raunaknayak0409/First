5740b2e6dbf6894322a2899e626c7218eaadcdf6
satyavrat49 <satyavrat49@gmail.com>
2016-03-14 23:09:28 +0530

    Initial commit

---
+++ src/org/raunak/FirstClass.java
@@ -0,0 +1,12 @@
+package org.raunak;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world");
+		
+	}
+
+}
